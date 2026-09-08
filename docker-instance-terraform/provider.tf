4abd32ece5c4653b9915de736c3cd310cc5baf49
Pavithra <pavithrakasukurthi00@gmail.com>
2026-09-08 16:37:53 +0530

    aws

--- docker-instance-terraform/provider.tf
+++ docker-instance-terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
 
     backend "s3" {
-        bucket = "pavi-module-demo"
+        bucket = "pavi-state-files"
         key = "docker-state"
         region = "us-east-1"
         use_lockfile = true
